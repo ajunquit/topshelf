9a66f3511d492df72b0e9ede91144b938a26a588
ajunquit <ajunquit@gmail.com>
2020-05-30 01:41:09 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace topshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
